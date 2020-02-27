c9de6107d4aa84a07c95af142f08b6f533727558
chenshan <chenshan@yonyou.com>
2020-02-27 10:24:10 +0800

    文档提交

---
+++ console命令知识点/App.vue
@@ -0,0 +1,45 @@
+<template>
+<div >
+  <el-button type="primary" @click="aaa" >1234</el-button>
+{{time}}
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        time:"wevdsffd",
+          items:[
+        {name:"达拉斯",value:'123'},
+        {name:"休斯顿",value:'455'}
+          ]
+      }
+    },
+    methods:{
+      aaa(){
+     this.time="12345678"
+      }
+    },
+    created(){ 
+
+    },
+    mounted(){ 
+    console.time("test-demo");
+    const str1 = "hello";
+    const str2 = "world";
+    console.log(str1, str2);
+    console.table(this.items)//打印出的数组对象数据在控制台是表格结构
+    console.log("%c%s","color:pink","打印的内容是粉色的")//console.log 可以打印带样式的文字 还有图片等等
+    var a=1200;
+    for(var i=0;i<a;i++){
+
+    }
+    console.timeEnd("test-demo"); //这个console.time 和console.timeEnd方法 必须传入相同参数 这个对测试同步的代码执行时间有帮助
+    console.log(Promise)//网上说log和dir没有太大区别 但是在现在的谷歌里 打印函数 log只是打印一个范范的函数 有种string化的感觉
+    console.dir(Promise)//而dir会打印这个函数的具体属性 方法等等....
+    },
+  }
+</script>
+<style>
+</style>
---
+++ vue生命周期/App.vue
@@ -0,0 +1,62 @@
+<template>
+<div >
+  <el-button type="primary" @click="aaa">1234</el-button>
+{{time}}
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+          time:new Date()
+      }
+    },
+    methods:{
+      aaa(){
+     this.time="12345678"
+      }
+    },
+    beforeCreate () { //此时vue实例还没有创建
+      console.log('%c%s','color:blue','beforeCreate的data内容'+this.time);
+    },
+    created(){ //此时 vue实例已经创建好 已经有data的监听和methods事件 
+    //之后判断有没有template 有就把data里的数据和模板里的vue语法翻译成html 没有 就把el元素及其后代(outHtml)作为template
+      console.log('%c%s','color:blue','created的data内容'+this.time);
+      this.time='3333' //此时data改变并不会出发 updated beforeUpdate 两个钩子函数 因为mounted之后才会实时监控data变化 
+    },
+    beforeMount () { //开始挂载编译生成的html到对应的位置 此时还没有将编译好的html渲染到页面上 之后会创建$el属性
+
+    },
+    mounted(){ //此时已经有$el属性 页面已经完成渲染 可以在此时发送ajax请求 该钩子函数在生命周期中只执行一次
+    
+    },
+    beforeUpdate () { //mounted执行的data数据监听 发生在虚拟 DOM 打补丁之前 可访问现有dom
+      console.log('%c%s','color:green','beforeUpdate的data内容')
+      console.log(this.$data)
+      console.log(JSON.stringify(this.$data))
+      console.log('%c%s','color:purple',"beforeUpdate更新前的dom内容")
+      console.log(this.$el);
+      console.log(this.$el.innerHTML)
+    },
+    updated(){ //数据更新后的状态 但最好不要在此做操作 而是用 watcher或者计算属性
+      console.log('%c%s','color:green','beforeUpdate的data内容')
+      console.log(this.$data)
+      console.log(JSON.stringify(this.$data))
+      console.log('%c%s','color:purple',"beforeUpdate更新前的dom内容")
+      console.log(this.$el);
+      console.log(this.$el.innerHTML)
+    },
+    beforeDestroy () { //vue实例销毁前的钩子函数
+      
+    },
+    destroyed () { //vue实例销毁执行的钩子函数
+      
+    }
+
+
+
+  }
+</script>
+<style>
+</style>
